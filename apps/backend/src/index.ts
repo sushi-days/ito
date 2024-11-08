8c76a80825ad3a5e720b9f1212be54b34e95d827
staticWagomU <to6wa17@gmail.com>
2024-11-08 21:33:59 +0900

    chore: change template cloudflare to nodejs

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,16 @@
-import { Hono } from "hono";
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
 
-const app = new Hono();
+const app = new Hono()
 
-app.get("/", (c) => {
-	return c.text("Hello Hono!");
-});
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
 
-export default app;
+const port = 3000
+console.log(`Server is running on http://localhost:${port}`)
+
+serve({
+  fetch: app.fetch,
+  port
+})
